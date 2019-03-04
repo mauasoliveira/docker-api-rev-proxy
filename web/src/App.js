d4129bec461f2fef5206729e18e71c143ef81c5b
Oliveira , Mauricio <mauricio.oliveira@mirumagency.com>
2019-03-04 12:24:31 -0300

    Support for Ajax

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    brands: []
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:3000/brands")
+      .then(res => res.json())
+      .then(result => this.setState({ brands: result }));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>&nbsp;</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.brands.map(x => {
+              return (
+                <tr key={x.id}>
+                  <td>{x.id}</td>
+                  <td>{x.name}</td>
+                  <td>&nbsp;</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     );
   }
